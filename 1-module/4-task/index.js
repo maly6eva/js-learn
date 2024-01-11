bbc4c72c74716566971b7b727fb9fb6873445333
Valery Malyshev <pherum@mail.ru>
2024-01-11 03:58:27 +0300

    task 4

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,10 @@
 function checkSpam(str) {
   // ваш код...
+  const lowerStr = str.toLowerCase();
+
+  if (lowerStr.includes('1xBet'.toLowerCase())) {
+    return true;
+  }
+
+  return lowerStr.includes('XXX'.toLowerCase());
 }
